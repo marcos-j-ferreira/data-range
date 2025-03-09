426f8b83a12b43e1a5b7fe017543ef0b26546748
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-03-09 20:26:38 -0300

    table

--- main.py
+++ main.py
@@ -79,9 +79,9 @@ data = {
     "up": [],
     "down": [],
     "range": {
-        "sinal" : "0%",
-        "TR": 0,
-        "TT": 0
+        "sinal" : [],
+        "TR": [],
+        "TT": []
     }
 }
 
@@ -90,15 +90,15 @@ def read_file(opetion, filename):
     if not os.path.exists(filename):
         print(f"not found: {filename}")
         return None
-    
-    padroes = {
-    "Taxa de recepção": r"Taxa de recep[\s\S]*?\(Mbps\)\s*:\s*(\d+)",
-    "Taxa de transmissão": r"Taxa de transmiss[\s\S]*?\(Mbps\)\s*:\s*(\d+)",
-    "Sinal": r"Sinal\s*:\s*(\d+)%"
-}
+
     
     if opetion == "netsh":
         
+        padroes = {
+            "Taxa de recepção": r"Taxa de recep[\s\S]*?\(Mbps\)\s*:\s*(\d+)",
+            "Taxa de transmissão": r"Taxa de transmiss[\s\S]*?\(Mbps\)\s*:\s*(\d+)",
+            "Sinal": r"Sinal\s*:\s*(\d+)%"
+        }
         try:
 
             result = {}
@@ -110,9 +110,9 @@ def read_file(opetion, filename):
                     if match:
                         result[c] = match.group(1)
             
-            data['range']['TT'] = result['Taxa de transmissão']
-            data['range']['TR'] = result["Taxa de recepção"]
-            data['range']['sinal'] = result["Sinal"]
+            data['range']['TT'].append(result['Taxa de transmissão'])
+            data['range']['TR'].append(result["Taxa de recepção"])
+            data['range']['sinal'].append(result["Sinal"])
         
         except Exception as e:
             print(f"Erro {e}")
@@ -165,6 +165,42 @@ def read_file(opetion, filename):
         
     return data
 
+"""
+Até aqui ele está, lendo os arquivos que conterá os dados e armazenando em um dic
+"""
 
+def set_data():
+    files = {
+        "down": ["down-01.txt", "down-02.txt", "down-03.txt"],
+        "up": ["up-01.txt", "up-02.txt", "up-03.txt"],
+        "netsh": ["netsh-01.txt","netsh-02.txt","netsh-03.txt" ]
+    }
 
-print(data)
+    for category, file_list in files.items():
+        for file_name in file_list:
+            read_file(category, file_name)
+
+def format_table(data):
+    set_data()
+
+    header = "+---------+----+------+----------------------------------------------------+"
+    title = "| Rodadas | Up | Down |                        Data                        |"
+    
+    num_rodadas = len(data["up"])
+    
+    rows = []
+    for i in range(num_rodadas):
+        up = data["up"][i]
+        down = data["down"][i]
+        sinal = data["range"]["sinal"][i]
+        tr = data["range"]["TR"][i]
+        tt = data["range"]["TT"][i]
+        
+        row = f"|    {i+1}    | {up}  |  {down}   | 'T. Rec.': '{tr}', 'T. Tra.': '{tt}', 'Sinal': '{sinal}' |"
+        rows.append(row)
+    
+    table = f"\nTabela de Resultados:\n{header}\n{title}\n{header}\n" + "\n".join(rows) + f"\n{header}"
+    
+    return table
+
+print(format_table(data))
